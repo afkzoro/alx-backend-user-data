7c3753ac87448f135f900f9127b258c763c5da68
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-25 13:44:27 +0100

    commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,16 +4,5 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    """_summary_
-
-    Args:
-        fields (_type_): _description_
-        redaction (_type_): _description_
-        message (_type_): _description_
-        separator (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
     pattern = '|'.join([f'(?<={field}=)[^{separator}]*' for field in fields])
     return re.sub(pattern, redaction, message)
