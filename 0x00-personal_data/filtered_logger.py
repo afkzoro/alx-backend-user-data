93752921339dd8bff0bd0c9de9d833720e6ca730
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-25 13:22:14 +0100

    commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-
+""" Task-0 """
 import re
 
 
 def filter_datum(fields, redaction, message, separator):
+    """ Filter datum """
     pattern = fr'({separator.join(fields)})=[^;]+'
     return re.sub(pattern, f'{redaction}={redaction}', message)
