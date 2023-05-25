f02ed6aef9183a5663f69f895a7aaad9a1d22519
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-25 13:54:04 +0100

    documentation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,5 +6,6 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
+    """ filter datum """
     pattern = '|'.join([f'(?<={field}=)[^{separator}]*' for field in fields])
     return re.sub(pattern, redaction, message)
