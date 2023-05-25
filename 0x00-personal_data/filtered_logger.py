af552cfb7cf8fbeb384c748f23bca97115b742b1
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-25 13:52:49 +0100

    annotate

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ Task-0 Filtered logger  """
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     pattern = '|'.join([f'(?<={field}=)[^{separator}]*' for field in fields])
     return re.sub(pattern, redaction, message)
