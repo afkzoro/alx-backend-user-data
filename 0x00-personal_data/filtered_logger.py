5cf6f47c570d6a80a22556cecd730b3d952a887a
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-05-25 15:11:00 +0100

    commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ from typing import List
 import logging
 import os
 import mysql.connector
+from mysql.connector.connection import MySQLConnection
 
 
 class RedactingFormatter(logging.Formatter):
@@ -53,7 +54,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> MySQLConnection:
     """  get_db method
     """
     username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
