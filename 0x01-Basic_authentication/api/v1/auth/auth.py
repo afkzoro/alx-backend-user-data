bb73e6c966026b205c8ef922cf0d545df2be87c8
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-06-01 19:17:48 +0100

    fix class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,7 +7,7 @@ import fnmatch
 
 
 class Auth:
-    """ Auth class
+    """ Auth class for server
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require_auth method
