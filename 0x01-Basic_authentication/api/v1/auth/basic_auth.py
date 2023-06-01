410d41556ceb6a3c71ebf0b01a37e37c18a2ff9b
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-06-01 19:24:16 +0100

    Basic auth doc fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+""" BasicAuth inherited class """
 from api.v1.auth.auth import Auth
 import base64
 from models.user import User
@@ -5,8 +7,12 @@ from typing import TypeVar
 
 
 class BasicAuth(Auth):
+    """ BasicAuth inherited class
+    """
     def extract_base64_authorization_header(self,
                                             authorization_header: str) -> str:
+        """ extract base 64
+            """
         if authorization_header is None or not isinstance(
                 authorization_header, str):
             return None
@@ -16,6 +22,8 @@ class BasicAuth(Auth):
 
     def decode_base64_authorization_header(
             self, base64_authorization_header: str) -> str:
+        """ decode base 64 method
+         """
         if base64_authorization_header is None or not isinstance(
                 base64_authorization_header, str):
             return None
@@ -28,6 +36,8 @@ class BasicAuth(Auth):
 
     def extract_user_credentials(
             self, decoded_base64_authorization_header: str) -> (str, str):
+        """ extract user credentials
+        """
         if decoded_base64_authorization_header is None or not isinstance(
                 decoded_base64_authorization_header, str):
             return None, None
@@ -38,6 +48,8 @@ class BasicAuth(Auth):
 
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ user_object from credentials
+        """
         if user_email is None or not isinstance(user_email, str):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
@@ -51,6 +63,8 @@ class BasicAuth(Auth):
         return user
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """ current user
+        """
         if request is None:
             return None
         auth_header = self.authorization_header(request)
