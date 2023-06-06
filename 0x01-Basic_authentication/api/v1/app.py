731dc4df91d9b252859542b6889c429d3d7a5c5b
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-06-06 11:33:47 +0100

    commit port

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -23,5 +23,5 @@ def not_found(error):
 
 if __name__ == "__main__":
     host = os.getenv("API_HOST", "0.0.0.0")
-    port = os.getenv("API_PORT", "5000")
+    port = os.getenv("API_PORT", "5100")
     app.run(host=host, port=port)
