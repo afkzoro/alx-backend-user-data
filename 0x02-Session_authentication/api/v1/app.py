885dcf9f712adc49e518572d8d6a632bde595e5a
Samuel Theophilus <samueltheophilus019@gmail.com>
2023-06-09 07:17:42 +0100

    Test V1

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -27,17 +27,8 @@ else:
 def before_request():
     """ before request method
     """
-    if auth is None:
-        return
-
-    excluded_paths = ["/api/v1/status/", "/api/v1/unauthorized/",
-                      "/api/v1/forbidden/"]
-    if request.path not in excluded_paths and auth.require_auth(
-            request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
+    if auth is not None:
+        request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
